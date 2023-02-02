687e85a18774cb83174326c603f1a2726f4edb8f
abbos0959 <abbosakromovich777@gmail.com>
2023-02-02 23:29:11 +0500

    start project

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+require("dotenv").config({ path: __dirname + "/.env" });
+
+const app = require("./middleware/app");
+
+const PORT = process.env.PORT || 3001;
+console.log(process.env.PORT);
+
+app.listen(PORT, () => {
+   console.log("server ishladi");
+});
